eeb45d7720b11afd248f6479086c4afd01eaf0cf
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2025-05-12 17:14:55 +0300

    fix

--- resources/views/details/product-details.blade.php
+++ resources/views/details/product-details.blade.php
@@ -34,48 +34,50 @@
                 </div>
             </div>
             <div class="order-xl-0 order-1 col-xl-4 col-md-12 col-sm-12 col-12 sd-pad-r">
-                <div class="rts-single-wized Categories">
-                    <div class="wized-header">
-                        <h2 class="title">{{__('products.other')}}</h2>
+                <div class="position-sticky">
+                    <div class="rts-single-wized Categories">
+                        <div class="wized-header">
+                            <h2 class="title">{{__('products.other')}}</h2>
+                        </div>
+                        <div class="wized-body">
+                            <ul class="single-categories">
+                                @foreach ($Other as $item)
+                                <li @if($item->id == $Page->id) class="active" @endif><a href="{{route('product',$item->getTranslatedAttribute('slug'))}}">{{$item->getTranslatedAttribute('title')}} <i class="far fa-long-arrow-right"></i></a></li>
+                                @endforeach
+                            </ul>
+                        </div>
                     </div>
-                    <div class="wized-body">
-                        <ul class="single-categories">
-                            @foreach ($Other as $item)
-                            <li @if($item->id == $Page->id) class="active" @endif><a href="{{route('product',$item->getTranslatedAttribute('slug'))}}">{{$item->getTranslatedAttribute('title')}} <i class="far fa-long-arrow-right"></i></a></li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-                @if($Page->gallery)
-                <div class="rts-single-wized download service">
-                    <div class="wized-header">
-                        <h2 class="h5 title">{{__('products.catalogue')}}</h2>
-                    </div>
-                    <div class="wized-body">
-                        @php
-                            $gallery = json_decode($Page->gallery);
-                        @endphp
-                        @foreach ($gallery as $item)
-                        <div class="single-download-area">
+                    @if($Page->gallery)
+                    <div class="rts-single-wized download service">
+                        <div class="wized-header">
+                            <h2 class="h5 title">{{__('products.catalogue')}}</h2>
+                        </div>
+                        <div class="wized-body">
                             @php
+                            $gallery = json_decode($Page->gallery);
+                            @endphp
+                            @foreach ($gallery as $item)
+                            <div class="single-download-area">
+                                @php
                                 $file = pathinfo($item->download_link);
                                 $extension = $file['extension'];
-                            @endphp
-                            @if($extension == 'pdf')
-                            <img src="/assets/images/service/icon/07.svg" alt="Business_downlaod">
-                            @else
-                            <img src="/assets/images/service/icon/08.svg" alt="Business_downlaod">
-                            @endif
-                            <div class="mid">
-                                <span class="title">{{$item->original_name}}</span>
+                                @endphp
+                                @if($extension == 'pdf')
+                                <img src="/assets/images/service/icon/07.svg" alt="Business_downlaod">
+                                @else
+                                <img src="/assets/images/service/icon/08.svg" alt="Business_downlaod">
+                                @endif
+                                <div class="mid">
+                                    <span class="title">{{$item->original_name}}</span>
+                                </div>
+                                <a target="_blank" class="rts-btn btn-primary" href="{{Voyager::image($item->download_link)}}"><i class="fal fa-arrow-right"></i></a>
                             </div>
-                            <a target="_blank" class="rts-btn btn-primary" href="{{Voyager::image($item->download_link)}}"><i class="fal fa-arrow-right"></i></a>
+                            @endforeach
+                            
                         </div>
-                        @endforeach
-
                     </div>
+                    @endif
                 </div>
-                @endif
             </div>
         </div>
     </div>
